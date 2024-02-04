2779c8d2e986fdeea44ddc3746992504caf2bdd6
Katie Yang <13hy22@queensu.ca>
2024-02-04 09:02:03 -0500

    Update ExampleList.tsx

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -3,9 +3,9 @@ import { Example } from "./Example";
 import styles from "./Example.module.css";
 
 const DEFAULT_EXAMPLES: string[] = [
-    "What is included in my Northwind Health Plus plan that is not in standard?",
-    "What happens in a performance review?",
-    "What does a Product Manager do?"
+    "How did Jack Ma found Alibaba?",
+    "What does Jack Ma think about financial technology?",
+    "马云对青年企业家有什么建议？"
 ];
 
 const GPT4V_EXAMPLES: string[] = [
