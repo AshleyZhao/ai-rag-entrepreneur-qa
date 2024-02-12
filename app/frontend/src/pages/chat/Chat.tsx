ef9a7c8b7db20d3fcc1d584a6c7e99ff9e0d3621
Katie Yang <13hy22@queensu.ca>
2024-02-11 20:53:06 -0500

    Update Chat.tsx

--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -301,8 +301,8 @@ const Chat = () => {
                     {!lastQuestionRef.current ? (
                         <div className={styles.chatEmptyState}>
                             <SparkleFilled fontSize={"120px"} primaryFill={"rgba(115, 118, 225, 1)"} aria-hidden="true" aria-label="Chat logo" />
-                            <h1 className={styles.chatEmptyStateTitle}>Chat with your data</h1>
-                            <h2 className={styles.chatEmptyStateSubtitle}>Ask anything or try an example</h2>
+                            <h1 className={styles.chatEmptyStateTitle}>Ask An Entrepreneur!</h1>
+                            <h2 className={styles.chatEmptyStateSubtitle}>Currently we've loaded interview transcripts from Jack Ma, Elon Musk, and Ma Hua Teng. Ask anything or try an example.</h2>
                             <ExampleList onExampleClicked={onExampleClicked} useGPT4V={useGPT4V} />
                         </div>
                     ) : (
