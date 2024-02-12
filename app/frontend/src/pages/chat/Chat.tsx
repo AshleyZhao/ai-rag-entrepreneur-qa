3469bb200443e95f75ade802b79716ba742e1953
Katie Yang <13hy22@queensu.ca>
2024-02-11 21:56:13 -0500

    Update Chat.tsx

--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -154,7 +154,7 @@ const Chat = () => {
         const translatedQuestion = await translate(question);
         console.log(translatedQuestion);
 
-        lastQuestionRef.current = question; // Turn this back into translatedQuestion
+        lastQuestionRef.current = translatedQuestion; // Turn this back into translatedQuestion
 
         error && setError(undefined);
         setIsLoading(true);
